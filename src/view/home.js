ebdd892d207b6a7aab713e465b333da8957cfdcf
betsiana <betsi18_12@hotmail.com>
2019-08-26 21:49:52 -0500

    cambios

--- src/view/home.js
+++ src/view/home.js
@@ -45,25 +45,84 @@ export default () => {
             </ol>
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                <img class="d-block w-100" src="./image/img-1.jpg" alt="First slide">
+                <img class="d-block w-100 img-desktop" src="./image/img-1.jpg" alt="First slide">
+                <img class="d-block w-100 img-mob" src="./image/img-1-mob.jpg" alt="First slide">
                 </div>
                 <div class="carousel-item">
-                <img class="d-block w-100" src="./image/img-2.jpg" alt="Second slide">
+                <img class="d-block w-100 img-desktop" src="./image/img-2.jpg" alt="Second slide">
+                <img class="d-block w-100 img-mob" src="./image/img-2-mob.jpg" alt="First slide">
                 </div>
                 <div class="carousel-item">
-                <img class="d-block w-100" src="./image/img-3.jpg" alt="Third slide">
+                <img class="d-block w-100 img-desktop" src="./image/img-3.jpg" alt="Third slide">
+                <img class="d-block w-100 img-mob" src="./image/img-3-mob.jpg" alt="First slide">
                 </div>
             </div>
             <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                <span class=" fas fa-arrow-alt-circle-left" aria-hidden="true"></span>
+                <span class="fas fa-chevron-left" aria-hidden="true"></span>
                 <span class="sr-only">Previous</span>
             </a>
             <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                <span class="fas fa-arrow-alt-circle-right" aria-hidden="true"></span>
+                <span class="fas fa-chevron-right" aria-hidden="true"></span>
                 <span class="sr-only">Next</span>
             </a>
         </div>
     </div>
+// Quienes somos
+
+    <div> 
+        <div>
+            <div class="box-que col-5">
+            <h3> ¿Que es Fussion? </h3>
+            <p class="p-somos"> FUSSION es una marca de productos de diseño con valor sostenible hechos
+            en base a la recuperación y transformación de bolsas plásticas recolectadas
+            dentro del proceso de reciclaje diario </p>
+            <p> Recuperamos bolsas plásticas como materia primaa para nuestros productos,
+            tenemos claro que cada diseño es más que solo una cartera, monedero o cartuchera hecho de reciclaje, 
+            pues detrás de cada uno hay un motivo, un concepto, hay menos bolsas en el mar </p>
+            </div>
+            <div class="col-5">
+                <img src="./image/tortuga.jpg" class="img-somos">
+                </div>
+            </div>
+            <div>
+            <div class="box-que col-5">
+            <h3> Misión </h3>
+            <p class="p-somos"> Somos una empresa de impacto y socialmente responsable que busca promover la moda
+            sostenible en el Perú, sensibilizar a la comunidad sobre el cuidado del medio ambiente
+            a través de una cultura de consumo responsable y consciente teniendo como eje 
+            principal las 3Rs del Reciclaje </p>
+            </div>
+            <div class="col-5">
+            <img src="./image/fotosface1.jpg" class="img-somos">
+            </div>
+            </div>
+            <div>
+            <div class="box-que col-5"> 
+            <h3> Visión </h3>
+            <p class="p-somos"> Buscamos ser la primera opción de productos responsables y sostenibles, ser reconocidos como
+            marca de impacto con sentido y seguir sumando esfuerzos por el desarrollo sostenible del Perú y
+            el mundo a través de nuestro trabajo. </p>
+            </div>
+            <div class="col-5">
+            <img src="./image/tortuga.jpg" class="img-somos">
+            </div>
+            </div>
+            <div>
+            <div class="box-que col-5">
+            <h3> Valores </h3>
+            <p class="p-somos">  Coherencia - Creatividad – Innovación - Ética - Sostenibilidad - Responsable </p>
+            </div>
+            <div class="col-5">
+                <img src="./image/save.jpg" class="img-somos">
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+// El corazon de fussion
     <footer>
     <div class="cont-footer">
         <div >
